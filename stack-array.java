bf7d06578192d0d6e9dbda230f0213e3598b6a9b
Oluwasegun Ige <mailshegze@gmail.com>
2023-07-03 10:15:16 +0100

    Update stack-array.java with comments

--- stack-array.java
+++ stack-array.java
@@ -1,12 +1,18 @@
 public class FixedCapacityStackOfStrings {
+  // creates instance variables
   private String[] s;
   private int N = 0;
 
+  // asking clients to supply capacity is a cheat as the client code is meant to be separate from the implementation
   public FixedCapacityStackOfStrings(int capacity) { s = new String[capacity]; }
 
+  // checks if the stack is empty by returning whether or not the size is 0
   public boolean isEmpty() { return N == 0; }
 
+  // pushes a new item to the top of the stack by using the current size to index into the array,
+  // saving the item, and incrementing the size
   public void push(String item) { s[N++] = item; }
 
+  // pops the most recent item from the stack by decrementing the size and indexing into the array
   public String pop() { return s[--N]; }
 }
