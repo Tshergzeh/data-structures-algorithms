1514841ae776ace621d2390a5c02552322ef0dea
Oluwasegun Ige <mailshegze@gmail.com>
2023-07-03 10:36:02 +0100

    Grow the array size when full length is reached
    If the array is full, create a new array of twice the size, and copy the items

--- stack-array.java
+++ stack-array.java
@@ -1,18 +1,25 @@
-public class FixedCapacityStackOfStrings {
+public class ResizingArrayStackOfStrings {
+
   // creates instance variables
   private String[] s;
   private int N = 0;
-
-  // asking clients to supply capacity is a cheat as the client code is meant to be separate from the implementation
-  public FixedCapacityStackOfStrings(int capacity) { s = new String[capacity]; }
+  
+  public ResizingArrayStackOfStrings() { s = new String[1]; } // start with an array size of 1
 
   // checks if the stack is empty by returning whether or not the size is 0
   public boolean isEmpty() { return N == 0; }
 
-  // pushes a new item to the top of the stack by using the current size to index into the array,
-  // saving the item, and incrementing the size
-  public void push(String item) { s[N++] = item; }
+  public void push(String item) { 
+    if (N == s.length) resize(2 * s.length); // if the array is full, call the resize method to double its size
+    s[N++] = item;  // use the current size to index into the new array, store the pushed item in that position, increment the size of the array
+  }
 
+  private void resize(int capacity) {
+    String[] copy = new String[capacity]; // create a new array of strings with the size passed as the method argument
+    for (int i = 0; i < N; i++)
+      copy[i] = s[i]; // store each of the items in the previous array into the new array
+    s = copy; // replace the previous array with the new 
+  
   public String pop() { 
     String item = s[--N]; // decrement the size of the array and use the new size to index into the array, storing the last item in the array
     s[N] = null; // remove the last item by changing its value to null so that the garbage collect can reclaim memory
