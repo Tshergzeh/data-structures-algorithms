2be9dbe8849673faaff62156354205437823a1ef
Oluwasegun Ige <mailshegze@gmail.com>
2023-07-03 17:14:15 +0100

    Use generics in place of strings

--- stack-linkedlist.java
+++ stack-linkedlist.java
@@ -1,11 +1,11 @@
-public class LinkedStackOfStrings
+public class Stack<Item>
 {
     public LinkedStackOfStrings() {
         // Constructor
         private Node first = null; // initialize the first item in the stack to point to null
 
         private class Node {
-	    String item;
+	    Item item;
 	    Node next;
         }
     }
@@ -13,7 +13,7 @@ public class LinkedStackOfStrings
     // method returns whether the stack is empty by checking whether the first item is null
     public boolean isEmpty() { return first == null; } 
 
-    public void push(String item) {
+    public void push(Item item) {
         Node oldfirst = first; // first save a pointer the first item in the list
         first = new Node(); // create a new node to be placed at the beginning of the list
 
@@ -22,8 +22,8 @@ public class LinkedStackOfStrings
         first.next = oldfirst;
     }
 
-    public String pop {
-        String item = first.item; // save the first item to be returned 
+    public Item pop {
+        Item item = first.item; // save the first item to be returned 
         first = first.next; // advance the pointer to the first item to point to the next item, leaving the previous first to be collected by the garbage collector
         return item; // return the saved item
     }
